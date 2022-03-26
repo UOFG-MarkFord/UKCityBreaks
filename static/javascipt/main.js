c24909b17d066344d0444b29b20fd16b45b0eac7
“Mark <2551132F@student.gla.ac.uk>
2022-03-26 22:30:00 +0000

    commit was broken reverted to previous build

--- static/javascipt/main.js
+++ static/javascipt/main.js
@@ -7,14 +7,3 @@
 	} );
 </script>
 	 
-<script>
-	function myMap() {
-	var mapProp = {
-	  center:new google.maps.LatLng(54.5,-4.5),
-	  mapTypeId: google.maps.MapTypeId.ROADMAP,
-	  zoom:5,
-	};
-	var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-	}
-	var marker = new google.maps.Marker({position: center, map:map})
-</script>
